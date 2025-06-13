a92e89a6fb2079f686f13a238712bfb8b44cdc63
Harrison Billings <harrison.billings@pelo.tech>
2025-06-13 11:43:26 -0700

    smaller tabs

--- init.lua
+++ init.lua
@@ -25,5 +25,5 @@ vim.cmd.colorscheme "catppuccin-macchiato"
 vim.opt.signcolumn = 'yes'
 vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
-vim.opt["tabstop"] = 4
-vim.opt["shiftwidth"] = 4
+vim.opt["tabstop"] = 2
+vim.opt["shiftwidth"] = 2
