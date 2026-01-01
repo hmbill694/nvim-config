39fb0937417a807ef47abe4d1c5274a2c8bae85e
Harrison Billings <harrison.billings@pelo.tech>
2026-01-01 11:30:12 -0700

    make lualine show cwd from root

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,14 @@
 return {
     'nvim-lualine/lualine.nvim',
-    opts = {},
+    opts = {
+        sections = {
+            lualine_c = {
+                {
+                    'filename',
+                    path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path, 3 = relative to home
+                }
+            }
+        }
+    },
     dependencies = { 'nvim-tree/nvim-web-devicons' }
 }
